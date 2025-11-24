38c26a3807f8d60b0bec37ac92d2bf0350ec5397
Erwan932 <etgg470@gmail.com>
2025-11-24 22:20:30 +0100

    Particule effect rajout
    Ajout d'un particule effect sur le Player quand il saut seulement

--- Assets/My_Content/Script/Player_Movement.cs
+++ Assets/My_Content/Script/Player_Movement.cs
@@ -6,6 +6,7 @@ public class Player_Movement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    public ParticleSystem smokeFX;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -25,11 +26,13 @@ public class Player_Movement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            smokeFX.Play();
         }
 
         if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            smokeFX.Play();
         }
 
         Flip();
