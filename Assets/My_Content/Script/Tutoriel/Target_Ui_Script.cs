62a7cabc038f98545059f4d10315082cf72a3715
Erwan932 <etgg470@gmail.com>
2025-12-11 14:52:44 +0100

    code gestion canvas

--- Assets/My_Content/Script/Tutoriel/Target_Ui_Script.cs
+++ Assets/My_Content/Script/Tutoriel/Target_Ui_Script.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
 
-public class Target_Ui_Script : MonoBehaviour
+public class Target_UI_Script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Références UI")]
+    public GameObject groupBackground;
+    public GameObject savedBackground;
+    public GameObject recruitableBackground;
+
+    // -------------------------
+    //  FONCTIONS INDIVIDUELLES
+    // -------------------------
+
+    public void ShowGroup(bool state = true)
     {
-        
+        if (groupBackground != null)
+            groupBackground.SetActive(state);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowSaved(bool state = true)
     {
-        
+        if (savedBackground != null)
+            savedBackground.SetActive(state);
+    }
+
+    public void ShowRecruitable(bool state = true)
+    {
+        if (recruitableBackground != null)
+            recruitableBackground.SetActive(state);
+    }
+
+    // -------------------------
+    //  FONCTIONS POUR N’AFFICHER QU’UN PANEL
+    // -------------------------
+
+    public void ShowOnlyGroup()
+    {
+        ShowGroup(true);
+        ShowSaved(false);
+        ShowRecruitable(false);
+    }
+
+    public void ShowOnlySaved()
+    {
+        ShowGroup(false);
+        ShowSaved(true);
+        ShowRecruitable(false);
+    }
+
+    public void ShowOnlyRecruitable()
+    {
+        ShowGroup(false);
+        ShowSaved(false);
+        ShowRecruitable(true);
+    }
+
+    // -------------------------
+    //  TOUT CACHER
+    // -------------------------
+
+    public void HideAll()
+    {
+        ShowGroup(false);
+        ShowSaved(false);
+        ShowRecruitable(false);
     }
 }
